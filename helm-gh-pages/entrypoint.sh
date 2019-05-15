5f3fed2a001bbced31385ef992b82d71d705c57f
Jeff Billimek <jeff@billimek.com>
2019-05-15 16:26:54 -0400

    adding more debugging

--- helm-gh-pages/entrypoint.sh
+++ helm-gh-pages/entrypoint.sh
@@ -9,23 +9,29 @@ init() {
 }
 
 lint() {
-    ct lint --chart-dirs . || exit $?
+  echo "Linting in $PWD"
+  ct lint --chart-dirs . || exit $?
 }
 
 package() {
-    helm package ${1} --destination /github/home/pkg/
+  helm package ${1} --destination /github/home/pkg/
 }
 
 push() {
-  git config user.email ${GITHUB_ACTOR}@users.noreply.github.com
-  git config user.name ${GITHUB_ACTOR}
-  git remote set-url origin ${REPOSITORY}
-  git checkout gh-pages
-  mv /github/home/pkg/*.tgz .
-  helm repo index . --url ${URL}
-  git add .
-  git commit -m "Publish Helm chart(s)"
-  git push origin gh-pages
+  if [[ $(find /github/home/pkg/ -type f -name "*.tgz") ]]; then
+    echo "Processing $(find /github/home/pkg/ -type f -name "*.tgz")"
+    git config user.email ${GITHUB_ACTOR}@users.noreply.github.com
+    git config user.name ${GITHUB_ACTOR}
+    git remote set-url origin ${REPOSITORY}
+    git checkout gh-pages
+    mv /github/home/pkg/*.tgz .
+    helm repo index . --url ${URL}
+    git add .
+    git commit -m "Publish Helm chart(s)"
+    git push origin gh-pages
+  else
+    echo "Nothing to package!"
+  fi 
 }
 
 REPOSITORY="https://${GITHUB_ACTOR}:${GITHUB_TOKEN}@github.com/${GITHUB_REPOSITORY}.git"
@@ -42,8 +48,11 @@ fi
 
 init
 
+lint
+
 for chart_dir in $(ct list-changed --chart-dirs .); do
   # CHART=$(basename "$chart_dir")
+  echo "Processing $chart_dir"
   package "$chart_dir"
 done
 
