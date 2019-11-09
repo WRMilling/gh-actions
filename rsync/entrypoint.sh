bd5aa2a0b5be3bfbfdaa3c2e09bc8c2ec17355a4
Winston R. Milling <Winston@Milli.ng>
2019-11-09 18:56:30 -0500

    Add Known Hosts

--- rsync/entrypoint.sh
+++ rsync/entrypoint.sh
@@ -7,6 +7,10 @@ set -e
 
 SSH_PATH="$HOME/.ssh"
 mkdir "$SSH_PATH"
+
+ssh-keyscan -H $SERVER_NAME >> $SSH_PATH/known_hosts
+chmod 644 $SSH_PATH/known_hosts
+
 echo "$DEPLOY_KEY" > "$SSH_PATH/deploy_key"
 chmod 600 "$SSH_PATH/deploy_key"
 
