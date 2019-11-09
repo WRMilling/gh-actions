1894d749224fce41ae6d4e2a4937f63cf0abf80e
Winston R. Milling <Winston@Milli.ng>
2019-11-09 18:44:13 -0500

    Add rsync deploy action

---
+++ rsync/entrypoint.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+set -o errexit
+set -o nounset
+set -o pipefail
+set -e
+
+SSH_PATH="$HOME/.ssh"
+mkdir "$SSH_PATH"
+echo "$DEPLOY_KEY" > "$SSH_PATH/deploy_key"
+chmod 600 "$SSH_PATH/deploy_key"
+
+rsync -az --delete --progress -e "ssh -p22" $LOCAL_DIR $USERNAME@$SERVER_NAME:$REMOTE_DIR
