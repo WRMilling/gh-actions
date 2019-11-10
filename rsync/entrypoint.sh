45734ace80da5addbe6fd42fdea0eb89ab97f4b9
Winston R. Milling <Winston@Milli.ng>
2019-11-09 19:10:23 -0500

    Disable host key checking

--- rsync/entrypoint.sh
+++ rsync/entrypoint.sh
@@ -8,10 +8,7 @@ set -e
 SSH_PATH="$HOME/.ssh"
 mkdir "$SSH_PATH"
 
-ssh-keyscan -H $SERVER_NAME > "$SSH_PATH/known_hosts"
-chmod 600 "$SSH_PATH/known_hosts"
-
 echo "$DEPLOY_KEY" > "$SSH_PATH/deploy_key"
 chmod 600 "$SSH_PATH/deploy_key"
 
-rsync -az --delete --progress -e "ssh -p22 -i $SSH_PATH/deploy_key" $LOCAL_DIR $USERNAME@$SERVER_NAME:$REMOTE_DIR
+rsync -az --delete --progress -e "ssh -p22 -i $SSH_PATH/deploy_key -o StrictHostKeyChecking=no" $LOCAL_DIR $USERNAME@$SERVER_NAME:$REMOTE_DIR
