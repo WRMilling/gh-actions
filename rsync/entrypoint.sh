b3210310db59fcfaa41428e4d3c2bd2349dad809
Winston R. Milling <Winston@Milli.ng>
2020-11-20 13:11:01 -0600

    Add port as an option on rsync action
    Default to 22

--- rsync/entrypoint.sh
+++ rsync/entrypoint.sh
@@ -5,10 +5,12 @@ set -o nounset
 set -o pipefail
 set -e
 
+PORT="${PORT-22}"
+
 SSH_PATH="$HOME/.ssh"
 mkdir "$SSH_PATH"
 
 echo "$DEPLOY_KEY" > "$SSH_PATH/deploy_key"
 chmod 600 "$SSH_PATH/deploy_key"
 
-rsync -az --delete --progress -e "ssh -p22 -i $SSH_PATH/deploy_key -o StrictHostKeyChecking=no" $LOCAL_DIR $USERNAME@$SERVER_NAME:$REMOTE_DIR
+rsync -az --delete --progress -e "ssh -p $PORT -i $SSH_PATH/deploy_key -o StrictHostKeyChecking=no" $LOCAL_DIR $USERNAME@$SERVER_NAME:$REMOTE_DIR
